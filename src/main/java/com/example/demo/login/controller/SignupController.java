f40e042e7fc8a020b7b9f7fcf0bb8c2c623a147c
Naoya Iijima <naoyaiijima@hiroshima-u.ac.jp>
2019-09-05 07:13:51 +0000

    6.3.2途中

--- src/main/java/com/example/demo/login/controller/SignupController.java
+++ src/main/java/com/example/demo/login/controller/SignupController.java
@@ -8,12 +8,13 @@ import com.example.demo.login.domain.model.SignupForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class SignupController {
+public class SignupController<ValidatedSignupForm> {
 
     //ラジオボタン用変数
     private Map<String, String> radioMarriage;
@@ -52,7 +53,7 @@ public class SignupController {
      * ユーザー登録画面のPOSTメソッド用処理.
      */
     @PostMapping("/signup")
-    public String postSignUp(@ModelAttribute SignupForm form, 
+    public String postSignUp(@ModelAttribute @Validated SignupForm form, 
                                 BindingResult bindingResult, Model model) {
                                     
         if(bindingResult.hasErrors()){
